012444fd524f06503e37650d7adfc440e48d977d
Ittisafur <ittisafur@gmail.com>
2023-02-25 13:06:25 +0600

    Remove App Dir

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  swcMinify: true
 }
 
 module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>Hello there</div>
+  )
+}
+
+export default Index ;
